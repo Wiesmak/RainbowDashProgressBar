70478cfea416f8b0db1329a8091f5b27bc3214cd
Gerard Gajda <gerard.gajda@gmail.com>
2022-12-19 16:47:49 +0100

    Backward compatibility

--- src/Icons.java
+++ src/Icons.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+import java.util.Objects;
 
 public interface Icons {
-    ImageIcon DASHIE = new ImageIcon(Icons.class.getResource("/dashie/rsz_dashie.gif"));
+    ImageIcon DASHIE = new ImageIcon(Objects.requireNonNull(Icons.class.getResource("/dashie/rsz_dashie.gif")));
     // TODO: replace with the waiting one
-    ImageIcon WAIT = new ImageIcon(Icons.class.getResource("/dashie/rsz_dashie.gif"));
+    ImageIcon WAIT = new ImageIcon(Objects.requireNonNull(Icons.class.getResource("/dashie/rsz_dashie.gif")));
 //    ImageIcon WAIT = new ImageIcon(Icons.class.getResource("/rsz_dashie_wait.gif"));
 //    ImageIcon RWAIT = new ImageIcon(Icons.class.getResource("/rsz_rdashie_wait.gif"));
 }
