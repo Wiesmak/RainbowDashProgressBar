fff05adcb38c7db5c5a40f4a8fc24a4ebadafee1
Gerard Gajda <gerard.gajda@gmail.com>
2022-12-19 23:13:35 +0100

    Left flight Awesome!

--- src/Icons.java
+++ src/Icons.java
@@ -4,7 +4,8 @@ import java.util.Objects;
 public interface Icons {
     ImageIcon DASHIE = new ImageIcon(Objects.requireNonNull(Icons.class.getResource("/dashie/rsz_dashie.gif")));
     // TODO: replace with the waiting one
-    ImageIcon WAIT = new ImageIcon(Objects.requireNonNull(Icons.class.getResource("/dashie/rsz_dashie.gif")));
+    ImageIcon SLOWRIGHT = new ImageIcon(Objects.requireNonNull(Icons.class.getResource("/dashie_wait/right/rsz_dashieslow.gif")));
+    ImageIcon SLOWLEFT = new ImageIcon(Objects.requireNonNull(Icons.class.getResource("/dashie_wait/left/rsz_rdashieslow.gif")));
 //    ImageIcon WAIT = new ImageIcon(Icons.class.getResource("/rsz_dashie_wait.gif"));
 //    ImageIcon RWAIT = new ImageIcon(Icons.class.getResource("/rsz_rdashie_wait.gif"));
 }
